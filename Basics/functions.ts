00c014a384fd3b49a98140ad5f19fcdb78e54eff
Rehan George <rehan.george@gmail.com>
2024-02-08 21:04:09 +0530

    Add variables and functions

---
+++ Basics/functions.ts
@@ -0,0 +1,10 @@
+function square(num: number) {
+    return num * num;
+}
+
+function greet(person: string) {
+    return `Hi there, ${person}!`;
+}
+
+const doSomething = (person: string, age: number, isFunny: boolean) => {};
+
--- Basics/variables.ts
+++ Basics/variables.ts
@@ -1,3 +1,15 @@
 let movieTitle: string = "Amadeus";
 let numCatLives: number = 9;
-let gameOver: boolean = false; 
+let gameOver: boolean = false;
+
+let thing: any = "hello";
+thing = 23;
+
+const movies = ["Amadeus", "The Godfather", "The Dark Knight"];
+let foundMovie: string;
+
+for (let movie in movies) {
+    if (movie === "The Godfather") {
+        foundMovie = movie;
+    }
+}
