f94dff5a36382ebad236b4fd26dd8ebc0dd8e10a
RehanGeorge <rehan.george@gmail.com>
2024-02-11 18:18:41 +0530

    Refactor isLeapYear

--- Basics/functionsExercise.ts
+++ Basics/functionsExercise.ts
@@ -29,10 +29,7 @@ console.log(twoFer("Rehan"));
 // hint - use modulo
 
 function isLeapYear(year: number): boolean {
-    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-        return true;
-    }
-    return false;
+    return ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0);
 }
 
 console.log(isLeapYear(2024));
