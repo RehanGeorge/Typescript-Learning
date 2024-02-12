a5931fb415f6ace053e849ce7ef93bb0bcecee0e
RehanGeorge <rehan.george@gmail.com>
2024-02-12 17:52:50 +0530

    Add Arrays and exercises

---
+++ Basics/arrays.ts
@@ -0,0 +1,13 @@
+const activeUsers: string[] = ["test"];
+const numbersArray = [1, 2, 3];
+
+type Point = {
+    x: number;
+    y: number;
+}
+
+const coords: Point[] = []
+coords.push({ x: 1, y: 2 });
+coords.push({ x: 3, y: 4 });
+
+const board: string[][] = [["O", "X", "O"], ["X", "O", "X"], ["O", "X", "O"]];
