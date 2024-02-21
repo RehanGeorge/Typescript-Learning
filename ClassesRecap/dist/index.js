e90daa5d4d4baa4cc18dd1181a22ccd5c2806c8c
RehanGeorge <rehan.george@gmail.com>
2024-02-21 14:06:05 +0530

    Recap Classes in JS

--- ClassesRecap/dist/index.js
+++ ClassesRecap/dist/index.js
@@ -1,2 +1,29 @@
-"use strict";
-console.log("It's Working");
+class Player {
+  static description = "This is a player class";
+  #score = 0;
+  constructor(first, last) {
+    this.first = first;
+    this.last = last;
+  }
+  introduce() {
+    console.log(`Hi my name is ${this.first} ${this.last} and I am a player`);
+  }
+  get score() {
+    return this.#score;
+  }
+  set score(score) {
+    this.#score = score;
+  }
+}
+
+class AdminPlayer extends Player {
+  constructor(first, last, powers) {
+    super(first, last);
+    this.powers = powers;
+  }
+  isAdmin = true;
+}
+
+const player1 = new Player("John", "Warrior");
+const admin1 = new AdminPlayer("John", "Doe", "Super Strength");
+console.log(admin1);
