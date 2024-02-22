5e07abf056b4ce2697289a10e5d9bc1beceb5b45
RehanGeorge <rehan.george@gmail.com>
2024-02-22 20:50:15 +0530

    Typescript class

--- ClassesRecap/src/index.ts
+++ ClassesRecap/src/index.ts
@@ -1,2 +1,8 @@
-console.log("It's Working");
-console.log("Yay");
+class Player {
+    first: string;
+    last: string;
+    constructor(first: string, last: string) {
+        this.first = first;
+        this.last = last;
+    }
+}
