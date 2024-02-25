dbbe5d89610b75065895d876564469b2626f284e
RehanGeorge <rehan.george@gmail.com>
2024-02-25 19:38:45 +0530

    Add Generic Type

--- Generics/dist/index.js
+++ Generics/dist/index.js
@@ -4,3 +4,23 @@ const colors = [];
 const inputEl = document.querySelector("#username");
 console.dir(inputEl.value);
 const btn = document.querySelector('.btn');
+function numberIdentity(item) {
+    return item;
+}
+function stringIdentity(item) {
+    return item;
+}
+// function identity(item: any): any {
+//     return item;
+// }
+function identity(item) {
+    return item;
+}
+identity("Hello");
+identity(7);
+function getRandomElement(list) {
+    const randIdx = Math.floor(Math.random() * list.length);
+    return list[randIdx];
+}
+console.log(getRandomElement(["a", "b", "c"]));
+console.log(getRandomElement([1, 2, 3]));
--- Generics/src/index.ts
+++ Generics/src/index.ts
@@ -6,3 +6,30 @@ const inputEl = document.querySelector<HTMLInputElement>("#username")!;
 console.dir(inputEl.value);
 
 const btn = document.querySelector<HTMLButtonElement>('.btn');
+
+function numberIdentity(item: number): number {
+    return item;
+}
+
+function stringIdentity(item: string): string {
+    return item;
+}
+
+// function identity(item: any): any {
+//     return item;
+// }
+
+function identity<Type>(item: Type): Type {
+    return item;
+}
+
+identity<string>("Hello");
+identity<number>(7);
+
+function getRandomElement<T>(list: T[]): T {
+    const randIdx = Math.floor(Math.random() * list.length);
+    return list[randIdx];
+}
+
+console.log(getRandomElement<string>(["a", "b", "c"]));
+console.log(getRandomElement<number>([1, 2, 3]));
