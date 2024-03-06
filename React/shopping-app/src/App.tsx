b256d5922dec9b15ed1b06f46d5ee0125c2cf733
RehanGeorge <rehan.george@gmail.com>
2024-03-06 22:21:23 +0530

    Complete React App

--- React/shopping-app/src/App.tsx
+++ React/shopping-app/src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import Greeter from './components/Greeter';
+import React, { useState } from 'react';
+import ShoppingList from './components/ShoppingList';
+import ShoppingListForm from './components/ShoppingListForm';
+import Item from './models/item';
+import { v4 as uuid } from 'uuid';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState<Item[]>([]);
+  const addItem = (product: string, quantity: number) => {
+    console.log("Made to the app component");
+    setItems([...items, {id: uuid(), product, quantity}])
+  }
+//   const items = [
+//     {id: 1, product: "Lemon", quantity:3 },
+//     {id: 2, product: "Milk", quantity:1},
+// ];
+
   return (
     <div className="App">
-      <Greeter />
+      <ShoppingList items={items}/>
+      <ShoppingListForm onAddItem={addItem}/>
     </div>
   );
 }
